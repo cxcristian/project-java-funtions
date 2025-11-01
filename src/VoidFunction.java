3364d16cebd70c4e87cac7fc40e027257cfb3fd6
Cristiandavid3d@gmail.com <Cristian>
2025-10-31 19:49:17 -0500

    chore: eliminar archivos del directorio src

--- src/VoidFunction.java
+++
@@ -1,9 +0,0 @@
-public class VoidFunction {
-    public static void main(String[] args) {
-        saludar();
-    }
-
-    public static void saludar() {
-        System.out.println("¡Hola, mundo!");
-    }
-}
